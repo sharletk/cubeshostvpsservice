5780b9aab525124685b93bc58bbf6bf5cf1a9aee
sharletp <sharletman@gmail.com>
2021-08-23 11:49:12 +0300

    fix(installer): Typos.

--- installer/main.sh
+++ installer/main.sh
@@ -7,35 +7,35 @@ conlog() {
 }
 
 coninfo() {
-  writeLog $1 LOG 97
+  writeLog $1 INFO 97
 }
 
 connotice() {
-  writeLog $1 LOG 96
+  writeLog $1 NOTICE 96
 }
 
 conwarn() {
-  writeLog $1 LOG 93
+  writeLog $1 WARN 93
 }
 
 conalert() {
-  writeLog $1 LOG 36
+  writeLog $1 ALERT 36
 }
 
 concritical() {
-  writeLog $1 LOG 95
+  writeLog $1 CRITICAL 95
 }
 
 conemergency() {
-  writeLog $1 LOG 41
+  writeLog $1 EMERGENCY 41
 }
 
 conerror() {
-  writeLog $1 LOG 98
+  writeLog $1 ERROR 98
 }
 
 condebug() {
-  writeLog $1 LOG 92
+  writeLog $1 DEBUG 92
 }
 
 conlogo() {
