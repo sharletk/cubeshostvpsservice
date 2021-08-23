815b6ccbf89a1b06541900efe64cc7cc56846365
sharletp <sharletman@gmail.com>
2021-08-23 13:23:13 +0300

    fix(maininstaller): Fixed issue with all arguments fetching.

--- installer/main.sh
+++ installer/main.sh
@@ -1,41 +1,42 @@
 writeLog() {
-  printf "\033[0m#\033[0;4m\033[0;1m\033[0;${3}m${2}\033[0m: ${1}\n\n" 
+  parsedData=( "${@:3} ")
+  printf "\033[0m#\033[0;4m\033[0;1m\033[0;${2}m${1}\033[0m: ${parsedData[*]}\n\n" 
 }
 
 conlog() {
-  writeLog $1 LOG 92
+  writeLog LOG 92 $*
 }
 
 coninfo() {
-  writeLog $1 INFO 97
+  writeLog INFO 97 $*
 }
 
 connotice() {
-  writeLog $1 NOTICE 96
+  writeLog NOTICE 96 $*
 }
 
 conwarn() {
-  writeLog $1 WARN 93
+  writeLog WARN 93 $*
 }
 
 conalert() {
-  writeLog $1 ALERT 36
+  writeLog ALERT 36 $*
 }
 
 concritical() {
-  writeLog $1 CRITICAL 95
+  writeLog CRITICAL 95 $*
 }
 
 conemergency() {
-  writeLog $1 EMERGENCY 41
+  writeLog EMERGENCY 41 $*
 }
 
 conerror() {
-  writeLog $1 ERROR 98
+  writeLog ERROR 98 $*
 }
 
 condebug() {
-  writeLog $1 DEBUG 92
+  writeLog DEBUG 92 $*
 }
 
 conlogo() {
