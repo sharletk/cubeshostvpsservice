72db770b5a5dd87f678321a4f1d95d3606860453
sharletp <sharletman@gmail.com>
2021-08-24 07:34:38 +0300

    fix(installers): Coreected dir link;

--- virtualization/docker/installer.sh
+++ virtualization/docker/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ### Main Installer ###
-INSTALLER_SCRIPT="cubeshostvpsservice/installer/main.sh"
+INSTALLER_SCRIPT="cubeshostvpsservice-master/installer/main.sh"
 . $INSTALLER_SCRIPT
 
 checkRoot
